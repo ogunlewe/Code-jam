548b58322f539b6a68b5d10be93df355de0223f6
Ogunlewe Al Ameen <ogunlewealameen08@gmail.com>
2024-10-09 10:29:07 +0100

    commit

--- Temp.java
+++
@@ -1,9 +0,0 @@
-// This is the class declaration. The class name must match the filename.
-public class sanke {
-
-    // This is the main method. It's the entry point of every Java program.
-    public static void main(String[] args) {
-        // This line prints "Hello, World!" to the console.
-        System.out.println("Hello, World!");
-    }
-}
--- temp.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
-g++ hello_world.cpp -o hello_world
-hello_world.exe
